de07d80f2bbfd5d83b06649953b0492acc2efe99
ciptacoding <ciptacoding@gmail.com>
2025-12-12 15:48:17 +0800

    Add variables and print them

--- main.go
+++ main.go
@@ -6,17 +6,17 @@ import (
 
 func main() {
 	// Create 3 variables with string, int, and boolean types
-	var name string = "John Doe"
-	var age int = 10
-	var isActive bool = true
+	var name string = "Raihana Rahiela"
+	var age int = 22
+	var myGirlfriend bool = true
 
 	// Print the variables using fmt.Printf with appropriate Go verbs
 	fmt.Printf("Name: %s\n", name)
 	fmt.Printf("Age: %d\n", age)
-	fmt.Printf("Is Active: %t\n", isActive)
+	fmt.Printf("Is Active: %t\n", myGirlfriend)
 
 	// Print the data types using %T verb
 	fmt.Printf("%T\n", name)
 	fmt.Printf("%T\n", age)
-	fmt.Printf("%T\n", isActive)
+	fmt.Printf("%T\n", myGirlfriend)
 }
